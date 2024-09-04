34d80ef6bc8d34af620d837b183caf08f7afd28e
Paul Tuck <paul.tuck@oxon.org>
2024-09-04 20:38:13 +0700

    import default Roboto font, theme provider and theme

--- web/src/theme/Root.tsx
+++ web/src/theme/Root.tsx
@@ -1,6 +1,15 @@
 import React, { ReactNode } from "react";
+// Import React Redux provider and global store
 import { Provider } from "react-redux";
-import store from "@site/src/redux/store"; // Adjust the path to your store
+import store from "@site/src/redux/store";
+// Import Material UI theme provider
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+// Import required fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 interface RootProps {
     children: ReactNode;
