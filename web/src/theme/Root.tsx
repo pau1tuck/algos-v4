d40f3a469eb6065ccefb675acc0effa970b5fcc6
Paul Tuck <paul.tuck@oxon.org>
2024-09-05 21:04:56 +0700

    install Material UI provider and Montserrat

--- web/src/theme/Root.tsx
+++ web/src/theme/Root.tsx
@@ -5,7 +5,13 @@ import store from "@site/src/redux/store";
 // Import Material UI theme provider
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import CssBaseline from "@mui/material/CssBaseline";
 // Import required fonts
+import "@fontsource/quicksand/500.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/ubuntu-mono/400.css";
+import "@fontsource/ubuntu-mono/700.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -16,7 +22,14 @@ interface RootProps {
 }
 
 const Root = ({ children }: RootProps) => {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {children}
+            </ThemeProvider>
+        </Provider>
+    );
 };
 
 export default Root;
--- web/src/theme/theme.ts
+++ web/src/theme/theme.ts
@@ -1,15 +1,49 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
+
+// Extend the typography variants in Material UI
+declare module "@mui/material/styles" {
+    interface TypographyVariants {
+        code: React.CSSProperties; // Add 'code' variant
+    }
+
+    // Allow configuration using `createTheme`
+    interface TypographyVariantsOptions {
+        code?: React.CSSProperties; // Add 'code' option
+    }
+}
+
+// Update the Typography's variant prop options
+declare module "@mui/material/Typography" {
+    interface TypographyPropsVariantOverrides {
+        code: true; // Add 'code' to the variant prop
+    }
+}
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2', // Example primary color
+    palette: {
+        primary: {
+            main: "#1976d2",
+            light: "#42a5f5",
+            dark: "#1565c0",
+        },
+        secondary: {
+            main: "#dc004e",
+            light: "#f06292",
+            dark: "#ad1457",
+        },
     },
-    secondary: {
-      main: '#dc004e', // Example secondary color
+    typography: {
+        fontFamily:
+            '"Montserrat", "Roboto", "Helvetica", "Arial", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+        h1: {
+            fontFamily:
+                '"Quicksand", "Roboto", "Helvetica", "Arial", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+            fontWeight: 500,
+        },
+        code: {
+            fontFamily: '"Ubuntu Mono", monospace',
+        },
     },
-  },
-  // You can customize typography, spacing, breakpoints, etc.
 });
 
 export default theme;
