36bb8e8e3d5062aebe7da6e56e610f1b4f4ef74f
Paul Tuck <paul.tuck@oxon.org>
2024-09-15 20:27:51 +0700

    improve root import type for ReactNode

--- web/src/theme/Root.tsx
+++ web/src/theme/Root.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from "react";
+import React from "react";
+import type { ReactNode } from "react";
 // Import React Redux provider and global store
 import { Provider } from "react-redux";
 import store from "@site/src/redux/store";
@@ -15,15 +16,15 @@ import "@fontsource/ubuntu-mono/400.css";
 import "@fontsource/ubuntu-mono/700.css";
 
 interface RootProps {
-    children: ReactNode;
+	children: ReactNode;
 }
 
 const Root = ({ children }: RootProps) => {
-    return (
-        <Provider store={store}>
-            <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </Provider>
-    );
+	return (
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
+		</Provider>
+	);
 };
 
 export default Root;
