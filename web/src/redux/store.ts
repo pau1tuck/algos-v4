39fff66bd02d8f01985fdeacd6a3888d5206f5ba
Paul Tuck <p6ul.tuck@gmail.com>
2024-09-19 23:03:30 +0700

    refactor: Repair store.ts imports to remove type errors

--- web/src/redux/store.ts
+++ web/src/redux/store.ts
@@ -1,8 +1,8 @@
 // src/redux/store.ts
 import { configureStore } from "@reduxjs/toolkit";
-import quizReducer from "./slices/quizSlice";
-import userProgressReducer from "./slices/userProgressSlice";
-import authReducer from "./slices/authSlice"; // Import auth slice
+import quizReducer from "@site/src/redux/slices/quizSlice";
+import userProgressReducer from "@site/src/redux/slices/userProgressSlice";
+import authReducer from "@site/src/redux/slices/authSlice"; // Import auth slice
 import { useDispatch } from "react-redux"; // Import useDispatch
 
 const store = configureStore({
