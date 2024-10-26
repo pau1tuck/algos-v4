0284b312c59922470d280ef7efc383f2e7e9ad7b
Paul Tuck <p6ul.tuck@gmail.com>
2024-10-27 00:52:49 +0700

    feat: Update Scorebar component with RiNumbersFill icon and adjust styling.
    - Remove the "FIXME" comment and set requiresAuth to false in the JavaScript course welcome page.
    - Updated the Scorebar component to use RiNumbersFill icon for displaying the score and adjusted the styling.

--- web/src/modules/scorebar/components/Scorebar.tsx
+++ web/src/modules/scorebar/components/Scorebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RiNumbersFill } from 'react-icons/ri';
 import { useSelector } from 'react-redux';
 
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
@@ -52,15 +53,8 @@ const Scorebar = () => {
 					width="100%"
 				>
 					<Box display="flex" alignItems="center">
-						<EmojiEventsIcon
-							fontSize="small"
-							sx={{ color: "#FFCC6F" }}
-						/>
-						<Typography
-							variant="body1"
-							component="div"
-							sx={{ ml: 1 }}
-						>
+						<RiNumbersFill fontSize="large" color="green" />
+						<Typography variant="body1" component="div" sx={{ ml: 1 }}>
 							Score: {score} {/* Updated to use 'score' */}
 						</Typography>
 					</Box>
@@ -68,39 +62,27 @@ const Scorebar = () => {
 					<Box display="flex" alignItems="center">
 						<FavoriteIcon fontSize="small" color="error" />
 						<Box sx={{ ml: 1, width: 100 }}>
-							<Typography variant="body1">
-								Health: {health}
-							</Typography>
+							<Typography variant="body1">Health: {health}</Typography>
 						</Box>
 					</Box>
 
 					<Box display="flex" alignItems="center">
 						<StarIcon fontSize="small" color="secondary" />
 						<Box sx={{ ml: 1, width: 100 }}>
-							<Typography variant="body1">
-								XP: {xp} / 420
-							</Typography>
+							<Typography variant="body1">XP: {xp} / 420</Typography>
 						</Box>
 					</Box>
 
 					<Box display="flex" alignItems="center">
 						<GradeIcon fontSize="small" color="warning" />
-						<Typography
-							variant="body1"
-							component="div"
-							sx={{ ml: 1 }}
-						>
+						<Typography variant="body1" component="div" sx={{ ml: 1 }}>
 							{grade?.title}
 						</Typography>
 					</Box>
 
 					<Box display="flex" alignItems="center">
 						<EmojiEventsIcon fontSize="small" color="action" />
-						<Typography
-							variant="body1"
-							component="div"
-							sx={{ ml: 1 }}
-						>
+						<Typography variant="body1" component="div" sx={{ ml: 1 }}>
 							{rank?.title}
 						</Typography>
 					</Box>
